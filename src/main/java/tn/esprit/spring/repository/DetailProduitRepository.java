2ef0df87d9d5f29298c5f298b6176ec85b5ce888
azzabihamza <hamza.azzabi@esprit.tn>
2021-11-01 19:01:22 +0100

    Add repository layer with all the DAO interfaces

--- src/main/java/tn/esprit/spring/repository/DetailProduitRepository.java
+++ src/main/java/tn/esprit/spring/repository/DetailProduitRepository.java
@@ -1,5 +1,9 @@
 package tn.esprit.spring.repository;
 
-public interface DetailProduitRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import tn.esprit.spring.DAO.DetailProduit;
+
+public interface DetailProduitRepository extends CrudRepository<DetailProduit, Long> {
 
 }
