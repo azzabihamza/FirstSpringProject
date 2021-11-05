4ab98323a78a8f227e73c108eaa9fe89d6d6ac44
azzabihamza <hamza.azzabi@esprit.tn>
2021-11-05 16:21:22 +0100

    implement JPQL methode in Service layer

--- src/main/java/tn/esprit/spring/service/ClientService.java
+++ src/main/java/tn/esprit/spring/service/ClientService.java
@@ -2,7 +2,9 @@ package tn.esprit.spring.service;
 
 import java.util.List;
 
+import tn.esprit.spring.DAO.CategorieClient;
 import tn.esprit.spring.DAO.Client;
+import tn.esprit.spring.DAO.Profession;
 
 public interface ClientService {
 
@@ -16,4 +18,11 @@ public interface ClientService {
 
 	Client retrieveClient(Long id);
 
+	List<Client> retrieveClientsProffession(Profession profession);
+
+	int updateClientCategorieByProffession(CategorieClient categorieClient, Profession profession);
+
+	void deleteClientByCategoryAndProffession(CategorieClient categorieClient, Profession profession);
+
+	List<Client> retrieveAllClientsBetweenDate(String date1, String date2);
 }
--- src/main/java/tn/esprit/spring/service/ClientServiceImpl.java
+++ src/main/java/tn/esprit/spring/service/ClientServiceImpl.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
+import tn.esprit.spring.DAO.CategorieClient;
 import tn.esprit.spring.DAO.Client;
+import tn.esprit.spring.DAO.Profession;
 import tn.esprit.spring.repository.ClientRepository;
 
 @Service
@@ -49,4 +51,23 @@ public class ClientServiceImpl implements ClientService {
 		return client;
 	}
 
+	@Override
+	public List<Client> retrieveClientsProffession(Profession profession) {
+		return clientRepository.retrieveClientsByProffession(profession);
+	}
+
+	@Override
+	public int updateClientCategorieByProffession(CategorieClient categorieClient, Profession profession) {
+		return clientRepository.updateClientCategorieByProffession(categorieClient, profession);
+	}
+
+	@Override
+	public void deleteClientByCategoryAndProffession(CategorieClient categorieClient, Profession profession) {
+		clientRepository.deleteClientByCategorieClientAndProfession(categorieClient, profession);
+	}
+
+	@Override
+	public List<Client> retrieveAllClientsBetweenDate(String date1, String date2) {
+		return clientRepository.retrieveClientsBetweenDates(date1, date2);
+	}
 }
