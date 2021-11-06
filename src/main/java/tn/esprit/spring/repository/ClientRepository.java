95bf644f7263ae8bd88204f49b7c9a48219efbcc
azzabihamza <hamza.azzabi@esprit.tn>
2021-11-06 17:48:56 +0100

    Add Custom Query to produitRepo

--- src/main/java/tn/esprit/spring/repository/ClientRepository.java
+++ src/main/java/tn/esprit/spring/repository/ClientRepository.java
@@ -24,10 +24,6 @@ public interface ClientRepository extends CrudRepository<Client, Long> {
     @Query("DELETE FROM Client c where c.categorieClient= :categorie and c.profession= :profession")
     Long deleteClientByCategorieClientAndProfession(@Param("categorie")CategorieClient categorieClient, @Param("profession")Profession profession);
 
-   /* @Modifying
-    @Query(value = "INSERT INTO Client (nom,prenom,dateNaissance,email,password,profession,categorieClient) VALUES (:nom,:prenom,:dateN,:email,:password,:profession,:categorieClient)")
-    void insertClient(@Param("nom")String nom,@Param("prenom")String prenom,@Param("dateN")String dateN,@Param("email")String email,@Param("password")String password,@Param("profession")Profession profession,@Param("categorieClient")CategorieClient categorieClient);
-    */
     @Query("select c from Client c where c.dateNaissance between :date1 and :date2")
     List<Client> retrieveClientsBetweenDates(@Param("date1")String date1,@Param("date2")String date2);
 }
