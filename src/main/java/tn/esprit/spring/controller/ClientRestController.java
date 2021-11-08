6bc620c4387bdfe692251c6a4a5a8e4b2891f420
azzabihamza <hamza.azzabi@esprit.tn>
2021-11-08 11:17:58 +0100

    implement getClient by id rest controller

--- src/main/java/tn/esprit/spring/controller/ClientRestController.java
+++ src/main/java/tn/esprit/spring/controller/ClientRestController.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import tn.esprit.spring.DAO.Client;
 import tn.esprit.spring.service.ClientService;
@@ -17,5 +18,10 @@ public class ClientRestController {
     @GetMapping("getAllClients")
     public List<Client> getClients(){
         return clientService.retrieveAllClients();
+    }
+
+    @GetMapping("getClient/{client-id}")
+    public Client retrieveClient(@PathVariable("client-id") long id){
+        return clientService.retrieveClient(id);
     }
 }
