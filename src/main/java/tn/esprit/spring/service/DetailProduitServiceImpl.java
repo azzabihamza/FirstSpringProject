28d97bc382deb067bb8a93f59801bd792f710fd1
azzabihamza <hamza.azzabi@esprit.tn>
2021-11-27 19:20:26 +0100

    update

--- src/main/java/tn/esprit/spring/service/DetailProduitServiceImpl.java
+++ src/main/java/tn/esprit/spring/service/DetailProduitServiceImpl.java
@@ -2,7 +2,6 @@ package tn.esprit.spring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tn.esprit.spring.DAO.entity.CategorieProduit;
 import tn.esprit.spring.DAO.entity.DetailProduitEntity;
 import tn.esprit.spring.repository.DetailProduitRepository;
 import tn.esprit.spring.repository.ProduitRepository;
